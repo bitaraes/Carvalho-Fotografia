085aa03e6fe72a1c408c03c740dc81131aad20cf
Igor <igor_oliveira02@hotmail.com>
2021-06-24 09:35:35 -0300

    melhoria nas transições e distribuição de itens por coluna

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 const colunas = [
     [
         "img/carvalho-0.jpg",
-        "img/carvalho-1.jpg",
         "img/carvalho-2.jpg",
-        "img/carvalho-3.jpg",
-        "img/carvalho-13.jpg",
-        "img/carvalho-4.jpg"
+        "img/carvalho-4.jpg",
+        "img/carvalho-6.jpg",
+        "img/carvalho-8.jpg",
+        "img/carvalho-12.jpg"
     ],
     [
         "img/carvalho-5.jpg",
-        "img/carvalho-6.jpg",
+        "img/carvalho-3.jpg",    
         "img/carvalho-7.jpg",
-        "img/carvalho-8.jpg",
         "img/carvalho-9.jpg",
-        "img/carvalho-20.jpg",
-        "img/carvalho-21.jpg",
+        "img/carvalho-11.jpg",
+        "img/carvalho-13.jpg",
+        "img/carvalho-13.jpg"
 
     ],
     [
-        "img/carvalho-10.jpg",
-        "img/carvalho-11.jpg",
+        "img/carvalho-16.jpg",
+        "img/carvalho-18.jpg",
         "img/carvalho-12.jpg",
-        "img/carvalho-13.jpg",
-        "img/carvalho-14.jpg"
+        "img/carvalho-14.jpg",
+        "img/carvalho-20.jpg",
+        "img/carvalho-2.jpg",
     ],
     [
         "img/carvalho-15.jpg",
-        "img/carvalho-16.jpg",
+        "img/carvalho-21.jpg",
         "img/carvalho-17.jpg",
-        "img/carvalho-18.jpg",
-        "img/carvalho-19.jpg"
+        "img/carvalho-13.jpg",
+        "img/carvalho-19.jpg",
+        "img/carvalho-5.jpg"
     ]
 ]
 function listarFotos() {
